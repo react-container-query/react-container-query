a2f10eae8c39496a15e26c2edd2c99935d0b7e6b
Daiwei <daiweilu123@gmail.com>
2015-12-24 14:25:58 -0500

    Add polyfill for rAF

--- src/raf.js
+++ src/raf.js
@@ -1,4 +1,30 @@
-// Put requestAnimationFrame polyfill here
+const vendors = ['ms', 'moz', 'webkit', 'o'];
+let lastTime = 0;
+let _requestAnimationFrame = window.requestAnimationFrame;
+let _cancelAnimationFrame = window.cancelAnimationFrame;
 
-export const requestAnimationFrame = window.requestAnimationFrame;
-export const cancelAnimationFrame = window.cancelAnimationFrame;
+for(let x = 0; x < vendors.length && !_requestAnimationFrame; x++) {
+  _requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+  _cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+                         window[vendors[x]+'CancelRequestAnimationFrame'];
+}
+
+if (!_requestAnimationFrame) {
+  _requestAnimationFrame = function (callback) {
+      const currTime = (new Date()).getTime();
+      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      const id = window.setTimeout(() => {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+  };
+}
+
+if (!_cancelAnimationFrame) {
+  _cancelAnimationFrame = (id) => clearTimeout(id);
+}
+
+// Somehow "export requestAnimationFrame" didn't work with babel?
+export {_requestAnimationFrame as requestAnimationFrame};
+export {_cancelAnimationFrame as cancelAnimationFrame};
