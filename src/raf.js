872c179d1f9bffd4edffae33bed37828c662bab3
Daiwei <daiweilu123@gmail.com>
2016-01-17 12:27:10 -0500

    Provide server mock for browser API

--- src/raf.js
+++ src/raf.js
@@ -1,31 +1,40 @@
-const vendors = ['ms', 'moz', 'webkit', 'o'];
-let lastTime = 0;
-let requestAnimationFrame = window.requestAnimationFrame;
-let cancelAnimationFrame = window.cancelAnimationFrame;
+let requestAnimationFrame;
+let cancelAnimationFrame;
 
-for(let x = 0; x < vendors.length && !requestAnimationFrame; x++) {
-  requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-  cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
-                         window[vendors[x]+'CancelRequestAnimationFrame'];
+if (typeof document === 'undefined') {
+  // For Node.js
+  requestAnimationFrame = function () {};
+  cancelAnimationFrame = function () {};
+} else {
+  const vendors = ['ms', 'moz', 'webkit', 'o'];
+  let lastTime = 0;
+
+  requestAnimationFrame = window.requestAnimationFrame;
+  cancelAnimationFrame = window.cancelAnimationFrame;
+
+  for(let x = 0; x < vendors.length && !requestAnimationFrame; x++) {
+    requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+                           window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!requestAnimationFrame) {
+    requestAnimationFrame = function (callback) {
+      const currTime = (new Date()).getTime();
+      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      const id = window.setTimeout(() => {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!cancelAnimationFrame) {
+    cancelAnimationFrame = (id) => clearTimeout(id);
+  }
 }
 
-if (!requestAnimationFrame) {
-  requestAnimationFrame = function (callback) {
-    const currTime = (new Date()).getTime();
-    const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-    const id = window.setTimeout(() => {
-      callback(currTime + timeToCall);
-    }, timeToCall);
-    lastTime = currTime + timeToCall;
-    return id;
-  };
-}
-
-if (!cancelAnimationFrame) {
-  cancelAnimationFrame = (id) => clearTimeout(id);
-}
-
-// Somehow "export requestAnimationFrame" didn't work with babel?
 export {
   requestAnimationFrame,
   cancelAnimationFrame
