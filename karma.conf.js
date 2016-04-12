9d0d3934b9965b3eea656874283c8b766341ce1b
Daiwei <daiweilu123@gmail.com>
2016-04-12 09:36:51 -0400

    Update browser testing config

--- karma.conf.js
+++ karma.conf.js
@@ -28,25 +28,25 @@ const customLaunchers = {
     base: 'SauceLabs',
     browserName: 'chrome',
     platform: 'Windows 10',
-    version: '46.0'
+    // version: '46.0'
   },
   sl_firefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
     platform: 'Windows 10',
-    version: '42.0'
+    // version: '42.0'
   },
   sl_safari_8: {
     base: 'SauceLabs',
     browserName: 'safari',
     platform: 'OS X 10.10',
-    version: '8.0'
+    version: '8'
   },
   sl_safari_9: {
     base: 'SauceLabs',
     browserName: 'safari',
     platform: 'OS X 10.11',
-    version: '9.0'
+    version: '9'
   },
   sl_ios_safari_7: {
     base: 'SauceLabs',
@@ -84,25 +84,25 @@ const customLaunchers = {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 7',
-    version: '9.0'
+    version: '9'
   },
   sl_ie_10: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 8',
-    version: '10.0'
+    version: '10'
   },
   sl_ie_11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
-    version: '11.0'
+    version: '11'
   },
   sl_ie_edge: {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
-    version: '20.10240'
+    // version: '20.10240'
   }
 };
 
