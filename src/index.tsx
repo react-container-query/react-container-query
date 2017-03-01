1acbc9921a11df6629107e3481526ef67a6f9815
Jess Telford <jtelford@fairfaxmedia.com.au>
2017-03-01 11:19:38 +1100

    Support server side rendering with values prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import React = require('react');
 import ReactDOM = require('react-dom');
-import {Props, State, Params, Query} from './interfaces';
+import omitBy = require('lodash/omitBy');
+import matchQueries from 'container-query-toolkit/lib/matchQueries';
+import {Props, State, Params, Query, Values} from './interfaces';
 import ContainerQueryCore from './ContainerQueryCore';
 
+function cleanParams(query: Query, params: Params, width: number, height: number): Params {
+  // Clean up any matches that include a query for a value we don't yet have
+  if (width === Infinity) {
+    params = omitBy(params, (_, key: string): boolean => (
+      typeof query[key].minWidth !== 'undefined'
+      || typeof query[key].maxWidth !== 'undefined'
+    )) as Params;
+  }
+
+  if (height === Infinity) {
+    params = omitBy(params, (_, key: string): boolean => (
+      typeof query[key].minHeight !== 'undefined'
+      || typeof query[key].maxHeight !== 'undefined'
+    )) as Params;
+  }
+
+  return params;
+}
+
 /**
- * <ContainerQuery query={query}>
+ * <ContainerQuery query={query} values={{width: 123, height: 456}}>
  *   {(params) => {
  *     <div className={classname(params.class)}></div>
  *   }}
@@ -16,16 +37,27 @@ export class ContainerQuery extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = {
-      params: {}
-    };
+    const {width = Infinity, height = Infinity}: {width?: number, height?: number} = (props.values || {});
+
+    if (width === Infinity && height === Infinity) {
+      this.state = {
+        params: {}
+      };
+      return;
+    }
+
+    let params: Params = matchQueries(props.query)({width, height});
+
+    params = cleanParams(props.query, params, width, height);
+
+    this.setState({params});
   }
 
   componentDidMount() {
     this.cqCore = new ContainerQueryCore(this.props.query, (params) => {
       this.setState({params});
     });
-
+    this.cqCore.storeParams(this.state.params);
     this.cqCore.observe(ReactDOM.findDOMNode(this));
   }
 
@@ -49,7 +81,8 @@ export class ContainerQuery extends React.Component<Props, State> {
 
 export function applyContainerQuery<P extends {containerQuery: Params}>(
   Component: React.ComponentClass<P>,
-  query: Query
+  query: Query,
+  values?: Values
 ): React.ComponentClass<P> {
   class ContainerQuery extends React.Component<P, State> {
     static displayName: string = Component.displayName
@@ -60,16 +93,28 @@ export function applyContainerQuery<P extends {containerQuery: Params}>(
 
     constructor(props: P) {
       super(props);
-      this.state = {
-        params: {}
-      };
+
+      const {width = Infinity, height = Infinity}: {width?: number, height?: number} = (values || {});
+
+      if (width === Infinity && height === Infinity) {
+        this.state = {
+          params: {}
+        };
+        return;
+      }
+
+      let params: Params = matchQueries(query)({width, height});
+
+      params = cleanParams(query, params, width, height);
+
+      this.setState({params});
     }
 
     componentDidMount() {
       this.cqCore = new ContainerQueryCore(query, (params) => {
         this.setState({params});
       });
-
+      this.cqCore.storeParams(this.state.params);
       this.cqCore.observe(ReactDOM.findDOMNode(this));
     }
 
