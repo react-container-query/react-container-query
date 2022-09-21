dde542232d06bdaac6efa580c853a1d03bfefc36
Pixelbandito <pixelbandito+ask@gmail.com>
2022-09-21 00:13:00 -0500

    Update Sauce Labs config to test more up-to-date version of Safari for iOS

--- config/saucelabs-browsers.js
+++ config/saucelabs-browsers.js
@@ -17,19 +17,12 @@ module.exports = {
     platform: 'OS X 10.15',
     version: '13'
   },
-  sl_ios_safari_10: {
+  sl_ios_safari_16: {
     base: 'SauceLabs',
     browserName: 'safari',
     deviceName: 'iPhone Simulator',
     platform: 'iOS',
-    version: '10.3'
-  },
-  sl_ios_safari_11: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    deviceName: 'iPhone Simulator',
-    platform: 'iOS',
-    version: '11.0'
+    version: '16.1'
   },
   // Disable Android testing because I don't know how to get it to work with
   // SauceLabs
